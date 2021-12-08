6820d52d99606c11ac9a1e71642690ebb66a8c77
Amal <amal.guliev@inbox.ru>
2021-12-08 17:58:07 +0400

    exercise 1

---
+++ hw01/main.js
@@ -0,0 +1,19 @@
+let temperatureTf = 7
+temperatureTf = 7 * 9 / 5 + 32
+alert(temperatureTf + '°F')
+
+let admin
+let name = 'Василий'
+admin = name
+console.log(admin)
+
+let numberJs = 1000
+numberJs = 1000 + '108'
+console.log(numberJs)
+
+
+
+function temperatureConversion(valNum) {
+    document.getElementById('temperature__Сonversion').innerHTML = (valNum * 9 / 5) + 32;
+
+}
